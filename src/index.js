321453cabf514ccc70158279ddac45e54c6c1588
Sveta K <test@test.test>
2020-10-05 17:32:50 +0100

    init: start and finish "reverse int" task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let number = Math.abs(n);
+    let reversedNumber = 0;
+
+    while (number > 0) {
+        let lastPart = number % 10
+        number = Math.floor(number / 10);
+        reversedNumber = (reversedNumber * 10) + lastPart;
+    }
+
+    return reversedNumber;
 }
